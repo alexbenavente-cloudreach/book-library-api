abadb1d7c714fe0acdfc7d1d203cd4950b206956
alexbenavente-cloudreach <alex.benavente@cloudreach.com>
2022-03-12 16:58:04 -0500

    Added API PUT method to update books

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import json
 
 app = Flask(__name__)
 
-books_db = [
+books = [
     {
         "id": 1,
         "Title": "Harry Potter and the Sorcerer's Stone", 
@@ -24,11 +24,7 @@ books_db = [
         "Title": "The Wonderful Wizard of Oz", 
         "Author": "L. Frank Baum", 
         "Year": "1900"
-        },
-     {
-        "id": 4
-        },   
-
+    }
 ]
 
 @app.route("/", methods=["GET"])
@@ -37,11 +33,11 @@ def home():
 
 @app.route("/books", methods=["GET"])
 def list_books():
-    return jsonify(books_db)
+    return jsonify(books)
 
 @app.route("/books/<int:book_id>", methods=["GET"])
 def get_book(book_id):
-    book = next((book for book in books_db if book["id"] == book_id), None)
+    book = next((book for book in books if book["id"] == book_id), None)
     if book:
         return jsonify(book)
     else:
@@ -50,36 +46,35 @@ def get_book(book_id):
 @app.route('/books', methods=["POST"])
 def create_book():
     book_data = request.get_json()
-    title = data.get("book title")
-    author = data.get("book author")
-    year = data.get("book year")
+    title = book_data.get("Title")
+    author = book_data.get("Author")
+    year = book_data.get("Year")
     book = {
-        "id": len(books_db)+1,
+        "id": len(books)+1,
         "Title": title, 
         "Author": author, 
         "Year": year
         }
-    books_db.append(book)
-    return jsonify(books_db), HTTPStatus.CREATED
+    books.append(book)
+    return jsonify(books), HTTPStatus.CREATED
 
 
-''' 
-@app.route('/books/<int:book_id>', methods=['PUT'])
+@app.route("/books/<int:book_id>", methods=["PUT"])
 def update_book(book_id):
-    book = next((book for book in books if book['id'] == book_id), None)
+    book = next((book for book in books if book["id"] == book_id), None)
     if not book:
         return "Book not found",HTTPStatus.NOT_FOUND
     
     book_data = request.get_json()
     book.update(
         {
-        "Title": book_data.get("book title"), 
-        "Author": book_data.get("book author"), 
-        "Year": book_data.get("book year")
+        "Title": book_data.get("Title"), 
+        "Author": book_data.get("Author"), 
+        "Year": book_data.get("Year")
         }
     )
     return jsonify(book)
-'''
+
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1")
