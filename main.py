12a48f1acda3336dc20720c46df9bc2226025e8e
alexbenavente-cloudreach <alex.benavente@cloudreach.com>
2022-03-12 04:33:19 -0500

    Added POST method to create new book

--- main.py
+++ main.py
@@ -24,7 +24,10 @@ books_db = [
         "Title": "The Wonderful Wizard of Oz", 
         "Author": "L. Frank Baum", 
         "Year": "1900"
-        }
+        },
+     {
+        "id": 4
+        },   
 
 ]
 
@@ -42,8 +45,41 @@ def get_book(book_id):
     if book:
         return jsonify(book)
     else:
-        return "Book not found",HTTPStatus.NOT_FOUND
+        return "Book not found", HTTPStatus.NOT_FOUND
 
+@app.route('/books', methods=["POST"])
+def create_book():
+    book_data = request.get_json()
+    title = data.get("book title")
+    author = data.get("book author")
+    year = data.get("book year")
+    book = {
+        "id": len(books_db)+1,
+        "Title": title, 
+        "Author": author, 
+        "Year": year
+        }
+    books_db.append(book)
+    return jsonify(books_db), HTTPStatus.CREATED
+
+
+''' 
+@app.route('/books/<int:book_id>', methods=['PUT'])
+def update_book(book_id):
+    book = next((book for book in books if book['id'] == book_id), None)
+    if not book:
+        return "Book not found",HTTPStatus.NOT_FOUND
+    
+    book_data = request.get_json()
+    book.update(
+        {
+        "Title": book_data.get("book title"), 
+        "Author": book_data.get("book author"), 
+        "Year": book_data.get("book year")
+        }
+    )
+    return jsonify(book)
+'''
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1")
